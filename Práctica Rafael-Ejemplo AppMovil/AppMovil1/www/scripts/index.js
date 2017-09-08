deb31f14ffadf04d647f0d0688dd2a9a3ac79d4d
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2017-09-08 11:50:48 -0300

    Práctica Apache Cordova
    Ejemplo de práctica con Apache Cordova - Semana 1

---
+++ Práctica Rafael-Ejemplo AppMovil/AppMovil1/www/scripts/index.js
@@ -0,0 +1,36 @@
+﻿// Si quiere una introducción sobre la plantilla En blanco, vea la siguiente documentación:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// Para depurar código al cargar la página en cordova-simulate o en dispositivos o emuladores Android: inicie la aplicación, establezca puntos de interrupción 
+// y ejecute "window.location.reload()" en la Consola de JavaScript.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Controlar la pausa de Cordova y reanudar eventos
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        // TODO: Cordova se ha cargado. Haga aquí las inicializaciones que necesiten Cordova.
+        document.getElementById("btnBuscar").addEventListener('click', BuscarUsuario, false);
+    };
+
+    function onPause() {
+        // TODO: esta aplicación se ha suspendido. Guarde el estado de la aplicación aquí.
+    };
+
+    function onResume() {
+        // TODO: esta aplicación se ha reactivado. Restaure el estado de la aplicación aquí.
+    };
+
+    function BuscarUsuario() {
+        var usuario = document.getElementById("txtNombre").value;
+        if (usuario == "") {
+            alert("Ingrese un usuario");
+        }
+        else  {
+            alert("Bienvenido: " + usuario);
+        }
+    };
+} )();

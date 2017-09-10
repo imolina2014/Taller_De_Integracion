7f708cde95815dc625365c1049be5c941a7b0589
Alexi Gonzalez <agonzalez2016@alu.uct.cl>
2017-09-09 22:15:30 -0300

    informacion y mapa

--- AplicacionMobile/AplicacionMobile/www/scripts/index.js
+++ AplicacionMobile/AplicacionMobile/www/scripts/index.js
@@ -5,13 +5,15 @@
 (function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
         // Controlar la pausa de Cordova y reanudar eventos
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
+        $('#btnMostrarMapa').click(getDatos);
+
         // TODO: Cordova se ha cargado. Haga aquí las inicializaciones que necesiten Cordova.
         var parentElement = document.getElementById('deviceready');
         var listeningElement = parentElement.querySelector('.listening');
@@ -27,4 +29,44 @@
     function onResume() {
         // TODO: esta aplicación se ha reactivado. Restaure el estado de la aplicación aquí.
     };
-} )();
+
+    function getDatos() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+            maxiumAge: 300000,
+            timeout: 10000,
+            enableHighAccuracy: true
+        });
+    }
+
+    function onSuccess(position) {
+        var CusLat = position.coords.latitude;
+        var CusLon = position.coords.longitude;
+
+        document.getElementById("txtLat").value = CusLat;
+        document.getElementById("txtLon").value = CusLon;
+
+        try {
+            var coords = new google.maps.LatLng(CusLat, CusLon);
+
+            var opciones = {
+                center: coords, zoom: 15
+            };
+
+            var mapa = new google.maps.Map(document.getElementById("map"), opciones);
+            var marcador = new google.maps.Marker({
+                position: coords,
+                map: mapa,
+                title: "Mi Ubicacion",
+                animation: google.maps.Animation.DROP
+            });
+        }
+        catch (err) {
+            console.log(err.message);
+        }
+    }
+
+    function onError(err) {
+        console.log("codigo de err;" + err.code + " msj=" + err.message);
+    }
+
+})();

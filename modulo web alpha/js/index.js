e9846fce88899103e3098d1f9c3b0c7166352aac
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2017-09-30 01:27:57 -0300

    Creación Formulario Ayuda
    Diseño de formulario sección ayuda. Se agregan estilos y directorio js y php para agregar funcionalidad con bases de datos.

---
+++ modulo web alpha/js/index.js
@@ -0,0 +1,26 @@
+function mostrar() {
+	$.ajax({
+		type: "POST",
+		url: "datos.php",
+		success: function(d) {
+			$("#datos").html(d);
+		}
+	});	
+}
+
+function guardar() {
+	var datos = {
+		"Nombre": $("#caja_nombre").val(),
+		"Edad": $("#caja_edad").val()
+	}
+
+	$.ajax({
+		type: "POST",
+		url: "guardar.php",
+		data: datos,
+		success: function(d) {
+			alert(d);
+			mostrar();
+		}
+	});
+}

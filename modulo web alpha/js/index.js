0444a92853c16d5434a8ecc66b5264f70df37d5d
MarceloMontalba17 <mmontalba2015@alu.uct.cl>
2017-10-08 00:24:16 +0200

    filtración de incidentes
    Definicion e implementacion de metodos de filtrado de incidentes

--- modulo web alpha/js/index.js
+++ modulo web alpha/js/index.js
@@ -64,3 +64,70 @@ function BuscarIncidente() {
 		}
 	});
 }
+
+function filtrar(id){
+	var etiqueta = document.getElementById(id);
+	etiqueta.parentNode.removeChild(etiqueta);
+ 
+	var categoria=document.getElementById("categoria");
+	var seleccion=categoria.options[categoria.selectedIndex].value;
+	
+	var delitos={
+		"0":"Robo",
+		"1":"Asesinato",
+		"2":"Robo-Intimidación",
+		"3":"Homicidio",
+		"4":"Allanamiento de morada",
+	};
+	var accidentes={
+		"0":"Choque",
+		"1":"Caida",
+		"2":"Quemaduras",
+		"3":"Intoxicación",
+		"4":"Electrocución",
+	};
+
+	var div=document.getElementById("contenedor");
+	var selec=document.createElement("select");
+	selec.setAttribute("id","tipo");
+	selec.setAttribute("style","width:200px;");
+	div.appendChild(selec);
+	
+	
+	var tipo=document.getElementById("tipo");
+	selec=document.createElement("option");
+	selec.setAttribute("value","Seleccionar Tipo");
+	selec.setAttribute("label","Seleccionar Tipo");
+	tipo.appendChild(selec);
+	
+	if (seleccion=="Accidente"){
+		var count=0;
+		for (var i in accidentes){
+			if(accidentes.hasOwnProperty(i)){
+				count++;
+			}
+		}
+
+		for(i=0;i<count;i++){
+			var opcion=document.createElement("option");
+			opcion.setAttribute("value",accidentes[i].toString());
+			opcion.setAttribute("label",accidentes[i].toString());
+			tipo.appendChild(opcion);
+		}
+	}
+	if(seleccion=="Delito"){
+		var count=0;
+		for (var i in delitos){
+			if(delitos.hasOwnProperty(i)){
+				count++;
+			}
+		}
+
+		for(i=0;i<count;i++){
+			var opcion=document.createElement("option");
+			opcion.setAttribute("value",delitos[i].toString());
+			opcion.setAttribute("label",delitos[i].toString());
+			tipo.appendChild(opcion);
+		}
+	}
+}

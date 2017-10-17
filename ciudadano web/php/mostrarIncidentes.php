eecbb1bf6dddb483d76019930142c8d47cf71f02
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2017-10-17 12:40:05 -0300

    Rectificación de Módulos
    Se agregan y quitan elementos correspondientes a páginas web del sitio.

--- ciudadano web/php/mostrarIncidentes.php
+++ ciudadano web/php/mostrarIncidentes.php
@@ -5,7 +5,59 @@
 
 	$sql = "SELECT CATEGORIA,TIPO,DESCRIPCION,FECHA,CALLE,AsText(COORDENADAS) FROM incidentes WHERE CATEGORIA='$categoria' AND TIPO='$tipo'";	
 	$resultado = mysqli_query($mysqli,$sql);
-
+    
+    echo "  <div class='row'>
+                <div class='col-md-6'>
+                    <table class='table' style='width=100%'>
+    				    <thead class='thead-inverse'>
+            			    <tr>
+            				    <th>#</th>
+            				    <th>TIPO</th>
+            				    <th>FECHA</th>
+            				    <th>UBICACIÓN</th>
+            				</tr>
+            			</thead>
+            			<tbody>";
+    $nfilas = mysqli_num_rows($resultado);
+	
+	if($nfilas > 0){
+	    $count = 1;
+		while($aDatos = mysqli_fetch_array($resultado)) {
+			echo "  <tr>    
+	                    <th scope='row'>$count</th>
+						<td>$aDatos[1]</td>
+						<td>$aDatos[3]</td>";
+			$CoorSep = $aDatos[5];
+        	$CoorSep = explode(" ", $CoorSep);
+        	$CoorSep[0]= explode("(", $CoorSep[0]);
+        	$CoorSep[1]= explode(")", $CoorSep[1]);
+        	$lat = $CoorSep[0][1];
+        	$lon = $CoorSep[1][0];
+            echo "	    <td><input type='image' src='./img/maps.png' onclick='initMap($lat,$lon)' width='50' height='50'></td>
+			        </tr>";
+				echo "  <script>
+        				    function initMap(latitud,longitud) {
+                                var uluru = {lat: latitud, lng: longitud};
+                                var map = new google.maps.Map(document.getElementById('map'), {
+                                  zoom: 16,
+                                  center: uluru
+                                });
+                                var marker = new google.maps.Marker({
+                                  position: uluru,
+                                  map: map
+                                });
+                            }
+        				</script>";
+			$count = $count + 1;
+		}
+		echo  "         </tbody>
+			        </table>
+			    </div><br>
+			    <div class='col-md-6' id='map'>
+			    </div>
+            </div>";
+	}
+    
     echo "	<table class='table' style='width=100%'>
 				<thead class='thead-inverse'>
 				    <tr>
@@ -44,7 +96,7 @@
         				    function initMap(latitud,longitud) {
                                 var uluru = {lat: latitud, lng: longitud};
                                 var map = new google.maps.Map(document.getElementById('map'), {
-                                  zoom: 4,
+                                  zoom: 16,
                                   center: uluru
                                 });
                                 var marker = new google.maps.Marker({

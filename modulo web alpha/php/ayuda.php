b61f48cfde0c4b8a57e45bd98f93960f90ff500a
Patricio riffo <patricionicolas@live.cl>
2017-10-17 03:05:00 -0400

    Envio Correo
    Pulida funcionalidad envio de correo en archivo JS y PHP.

--- modulo web alpha/php/ayuda.php
+++ modulo web alpha/php/ayuda.php
@@ -3,13 +3,18 @@
 	$email=$_POST["Email"];
 	$asunto=$_POST["Asunto"];
 	$comentario=$_POST["Comentario"];
-	
-	$mensaje="Este es un mensaje de prueba!";
-	if(mail($email,$asunto,$mensaje)){
+	//armaremos el texto.
+	$texto = "Nombre: ".$nombre."   Correo: ".$email. " \n".$comentario;
+
+	echo"<script>alert('$texto')</script>";
+
+	//if(mail("priffo2014@alu.uct.cl","asuntillo","Este es el cuerpo del mensaje")){
+	if(mail("priffo2014@alu.uct.cl",$asunto,$texto)){
+
 		echo"<script>alert('En un momento recibirá un mensaje!')</script>";
 	}
 	else{
-		echo"<script>alert('Ha ocurrido un error inesperado')</script>";
+		echo"<script>alert('Ha ocurrido un error inesperadoooo')</script>";
 	}
 	
 ?>

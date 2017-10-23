215343453c66ff828367573508272d23bc3447c6
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2017-10-22 21:38:44 -0300

    Solución Error Mapa

--- ciudadano web/php/mostrarIncidentes.php
+++ ciudadano web/php/mostrarIncidentes.php
@@ -4,14 +4,16 @@
 	$categoria = $_POST["Categoria"];
 	$tipo = $_POST["Tipo"];
 	$fecha=$_POST["Fecha"];
-     
-    if ($calle=="")$sql = "SELECT CATEGORIA,TIPO,DESCRIPCION,FECHA,CALLE,AsText(COORDENADAS) FROM incidentes WHERE CATEGORIA='$categoria' AND TIPO='$tipo' AND FECHA='$fecha'";
-    else $sql = "SELECT CATEGORIA,TIPO,DESCRIPCION,FECHA,CALLE,AsText(COORDENADAS) FROM incidentes WHERE CATEGORIA='$categoria' AND TIPO='$tipo' AND FECHA='$fecha' CALLE LIKE '$calle%'";
     
-    $resultado = mysqli_query($mysqli,$sql);
+    
+    if ($calle=="")$sql = "SELECT CATEGORIA,TIPO,DESCRIPCION,FECHA,CALLE,AsText(COORDENADAS) FROM incidentes WHERE CATEGORIA='$categoria' AND TIPO='$tipo' AND FECHA='$fecha'";
+    
+	else $sql = "SELECT CATEGORIA,TIPO,DESCRIPCION,FECHA,CALLE,AsText(COORDENADAS) FROM incidentes WHERE CATEGORIA='$categoria' AND TIPO='$tipo' AND FECHA='$fecha' AND CALLE LIKE '%$calle%'";
+	
+	$resultado = mysqli_query($mysqli,$sql);
     
     echo "  <div class='row'>
-                <div class='col-md-6'>
+                <div class='col-md-6' style='border-radius: 2px; border: 2px solid #73AD21; overflow: scroll;'>
                     <table class='table' style='width=100%'>
     				    <thead class='thead-inverse'>
             			    <tr>
@@ -22,6 +24,7 @@
             				</tr>
             			</thead>
             			<tbody>";
+            			
     $nfilas = mysqli_num_rows($resultado);
 	
 	if($nfilas > 0){
@@ -56,13 +59,14 @@
 		}
 		echo  "         </tbody>
 			        </table>
-			    </div><br>
-			    <div class='col-md-6' id='map'>
+			    </div>
+			    <div class='col-md-6' id='map' style='border-radius: 2px; border: 2px solid #73AD21;'><br>
+			        <img src='./img/maps.png' width='500' height='400'>
 			    </div>
             </div>";
 	}
     
-    echo "	<table class='table' style='width=100%'>
+/*    echo "	<table class='table' style='width=100%'>
 				<thead class='thead-inverse'>
 				    <tr>
 				      	<th>#</th>
@@ -75,6 +79,7 @@
 				    </tr>
 				 </thead>
 				 <tbody>";
+
 	$nfilas = mysqli_num_rows($resultado);
 	
 	if($nfilas > 0){
@@ -114,7 +119,7 @@
 		echo  " </tbody>
 			</table>
 		";
-	}
+	}*/
 	else {
 	    echo "<script> alert('No se encontraron datos') </script>";
 	}

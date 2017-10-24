ed458b5c8f5fda4ca04a00b8743f1ca706b54b15
MarceloMontalba17 <mmontalba2015@alu.uct.cl>
2017-10-24 22:59:59 +0200

    mostrar tabla completa de Incidentes

--- ciudadano web/php/mostrarIncidentes.php
+++ ciudadano web/php/mostrarIncidentes.php
@@ -66,61 +66,38 @@
             </div>";
 	}
     
-/*    echo "	<table class='table' style='width=100%'>
-				<thead class='thead-inverse'>
-				    <tr>
-				      	<th>#</th>
-				      	<th>CATEGORÍA</th>
-				      	<th>TIPO</th>
-				      	<th>DESCRIPCIÓN</th>
-				      	<th>FECHA</th>
-				      	<th>CALLE</th>
-				      	<th>UBICACIÓN</th>
-				    </tr>
-				 </thead>
-				 <tbody>";
-
-	$nfilas = mysqli_num_rows($resultado);
-	
-	if($nfilas > 0){
-	    $count = 1;
-		while($aDatos = mysqli_fetch_array($resultado)) {
-			echo "  <tr>    
-	                    <th scope='row'>$count</th>
-	                    <td>$aDatos[0]</td>
-						<td>$aDatos[1]</td>
-						<td>$aDatos[2]</td>
-						<td>$aDatos[3]</td>
-						<td>$aDatos[4]</td>
-			     ";
-			$CoorSep = $aDatos[5];
-        	$CoorSep = explode(" ", $CoorSep);
-        	$CoorSep[0]= explode("(", $CoorSep[0]);
-        	$CoorSep[1]= explode(")", $CoorSep[1]);
-        	$lat = $CoorSep[0][1];
-        	$lon = $CoorSep[1][0];
-				echo "	<td><input type='button' class='btn btn-success' value='Ver Mapa' onclick='initMap($lat,$lon)'></td>
-					</tr>";
-				echo "  <script>
-        				    function initMap(latitud,longitud) {
-                                var uluru = {lat: latitud, lng: longitud};
-                                var map = new google.maps.Map(document.getElementById('map'), {
-                                  zoom: 16,
-                                  center: uluru
-                                });
-                                var marker = new google.maps.Marker({
-                                  position: uluru,
-                                  map: map
-                                });
-                            }
-        				</script>";
-			$count = $count + 1;
-		}
-		echo  " </tbody>
-			</table>
-		";
-	}*/
 	else {
 	    echo "<script> alert('No se encontraron datos') </script>";
 	}
+
+	$sql="SELECT * FROM incidentes";
+	$resultado=mysqli_query($mysqli,$sql);
+	$nfilas=mysqli_num_rows($resultado);
+				        
+	if($nfilas>0){
+	    echo"
+	    <div class='row'>
+                <div class='col-md-12'>
+            	     <table class='table'>
+            				<thead class='thead-inverse'>
+            			        <tr>
+            				        <th>#</th>
+            				        <th>CATEGORÍA</th>
+            				        <th>TIPO</th>
+            				        <th>FECHA</th>
+            				        <th>CALLE</th>
+            				    </tr>
+		";
+	    while($aDatos = mysqli_fetch_array($resultado)) {
+			echo"<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+			    <tr>
+                <th>$aDatos[0]</th>
+                <th>$aDatos[4]</th>
+                <th>$aDatos[5]</th>
+				<th>$aDatos[3]</th>
+                <th>$aDatos[7]</th>
+                </tr>";
+		}
+		echo'</thead></table></div></div>';
+    	   }
 ?>

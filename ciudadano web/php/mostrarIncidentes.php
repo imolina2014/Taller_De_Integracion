ab6c9d3276dbf31a01a6d7ecad3e71b3e911bd71
MarceloMontalba17 <mmontalba2015@alu.uct.cl>
2017-10-31 12:02:58 -0300

    Modificación Filtrado Vista Incidentes

--- ciudadano web/php/mostrarIncidentes.php
+++ ciudadano web/php/mostrarIncidentes.php
@@ -11,8 +11,10 @@
 	else $sql = "SELECT CATEGORIA,TIPO,DESCRIPCION,FECHA,CALLE,AsText(COORDENADAS) FROM incidentes WHERE CATEGORIA='$categoria' AND TIPO='$tipo' AND FECHA='$fecha' AND CALLE LIKE '%$calle%'";
 	
 	$resultado = mysqli_query($mysqli,$sql);
-    
-    echo "  <div class='row'>
+    $nfilas = mysqli_num_rows($resultado);
+	
+	if($nfilas > 0){
+		echo "  <div class='row'>
                 <div class='col-md-6' style='border-radius: 2px; border: 2px solid #73AD21; overflow: scroll;'>
                     <table class='table' style='width=100%'>
     				    <thead class='thead-inverse'>
@@ -24,15 +26,12 @@
             				</tr>
             			</thead>
             			<tbody>";
-            			
-    $nfilas = mysqli_num_rows($resultado);
-	
-	if($nfilas > 0){
+						
 	    $count = 1;
 		while($aDatos = mysqli_fetch_array($resultado)) {
 			echo "  <tr>    
 	                    <th scope='row'>$count</th>
-						<td>$aDatos[1]</td>
+						<td><img src='img/icons/$aDatos[1].png' id='G_iconos'></td>
 						<td>$aDatos[3]</td>";
 			$CoorSep = $aDatos[5];
         	$CoorSep = explode(" ", $CoorSep);
@@ -40,21 +39,8 @@
         	$CoorSep[1]= explode(")", $CoorSep[1]);
         	$lat = $CoorSep[0][1];
         	$lon = $CoorSep[1][0];
-            echo "	    <td><input type='image' src='./img/maps.png' onclick='initMap($lat,$lon)' width='50' height='50'></td>
-			        </tr>";
-				echo "  <script>
-        				    function initMap(latitud,longitud) {
-                                var uluru = {lat: latitud, lng: longitud};
-                                var map = new google.maps.Map(document.getElementById('map'), {
-                                  zoom: 16,
-                                  center: uluru
-                                });
-                                var marker = new google.maps.Marker({
-                                  position: uluru,
-                                  map: map
-                                });
-                            }
-        				</script>";
+            echo "<td><input type='button' value='Ver Mapa' onclick='initMap($lat,$lon)' width='50' height='50' class='btn btn-primary'></td>
+			     </tr>";
 			$count = $count + 1;
 		}
 		echo  "         </tbody>
@@ -68,36 +54,6 @@
     
 	else {
 	    echo "<script> alert('No se encontraron datos') </script>";
+		include("Incidentes.php");
 	}
-
-	$sql="SELECT * FROM incidentes";
-	$resultado=mysqli_query($mysqli,$sql);
-	$nfilas=mysqli_num_rows($resultado);
-				        
-	if($nfilas>0){
-	    echo"
-	    <div class='row'>
-                <div class='col-md-12'>
-            	     <table class='table'>
-            				<thead class='thead-inverse'>
-            			        <tr>
-            				        <th>#</th>
-            				        <th>CATEGORÍA</th>
-            				        <th>TIPO</th>
-            				        <th>FECHA</th>
-            				        <th>CALLE</th>
-            				    </tr>
-		";
-	    while($aDatos = mysqli_fetch_array($resultado)) {
-			echo"<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-			    <tr>
-                <th>$aDatos[0]</th>
-                <th>$aDatos[4]</th>
-                <th>$aDatos[5]</th>
-				<th>$aDatos[3]</th>
-                <th>$aDatos[7]</th>
-                </tr>";
-		}
-		echo'</thead></table></div></div>';
-    	   }
 ?>

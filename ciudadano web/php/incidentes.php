554abd4dd2d3184811098693a39c59153b5e0d02
MarceloMontalba17 <mmontalba2015@alu.uct.cl>
2017-10-31 12:39:09 -0300

    Update incidentes.php

--- ciudadano web/php/incidentes.php
+++ ciudadano web/php/incidentes.php
@@ -1,13 +1,12 @@
 <?php
-	header('Content-Type: text/html; charset=UTF-8');
 	include("conex.php");
+	mysqli_query($mysqli,"SET NAMES 'utf8'");
 	$sql="SELECT * FROM incidentes";
 	$resultado=mysqli_query($mysqli,$sql);
 	$nfilas=mysqli_num_rows($resultado);
 				        
 	if($nfilas>0){
 	    echo"
-	    <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
 	    <div class='row'>
                 <div class='col-md-12'>
             	     <table class='table'>
@@ -15,18 +14,17 @@
             			        <tr>
             				        <th>#</th>
             				        <th>CATEGORÍA</th>
-            				        <th>TIPO</th>
+            				        <th><div style='margin-left:40px'>TIPO</div></th>
             				        <th>FECHA</th>
             				        <th>CALLE</th>
             				    </tr>
 		";
 	    while($aDatos = mysqli_fetch_array($resultado)) {
 			echo"
-				<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
 			    <tr>
                 <th>$aDatos[0]</th>
                 <th>$aDatos[4]</th>
-                <th>$aDatos[5]</th>
+                <th><img src='img/icons/$aDatos[5].png' id='iconos'> $aDatos[5]</th>
 				<th>$aDatos[3]</th>
                 <th>$aDatos[7]</th>
                 </tr>";

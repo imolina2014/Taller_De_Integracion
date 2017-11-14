8b9f5a0687492042487d9f34424ec0f774e58ac7
GonzaloGarrido <gonzalo.garrido2016@alu.uct.cl>
2017-11-13 23:19:38 -0300

    eliminando comentarios
    eliminacion de codigos basura

--- municipalidad web/RevisionSectores.php
+++ municipalidad web/RevisionSectores.php
@@ -28,34 +28,6 @@ session_start();
 		  <a href='login.php'>Salir  <?php echo $_SESSION['usuario']?></a>
 		  <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
 		</div>
-
-		<!--<header>
-			<nav class="navbar transparent navbar-inverse navbar-fixed-top" style="background-color:rgba(0,0,0,0.9);">
-				<div class='container-fluid'>
-					<div class='navbar-header'>
-						<button type='button' class='navbar-toggle collapsed navbar-fixed-top' data-toggle='collapse' data-target='#navbar-1'>
-							<span class='sr-only'>Menu</span>
-							<span class='icon-bar'></span>
-							<span class='icon-bar'></span>
-							<span class='icon-bar'></span>
-						</button>
-						<a href="index.php" class='navbar-brand'>SISTEMA DE INGRESO DE INCIDENTES</a>
-					</div>
-					<div class='collapse navbar-collapse'>
-						<ul class='nav navbar-nav'>
-						</ul>		
-						<ul class='nav navbar-nav navbar-right' id='navbar-1'>
-							<li><a href='incidentes.php'>Incidentes</a></li>
-							<li><a href='Estadisticas.php'>Estadisticas</a></li>
-							<li><a href='CreacionSectores.php'>Creacion Sectores</a></li>
-							<li class='active'><a href='RevisionSectores.php' style='color:black; background-color:white;' >Revision Sectores</a></li>
-							<li><a href='login.php' class='sesion'>Salir</a></li>
-						</ul>
-					</div>	
-				</div>
-			</nav>
-		</header>-->
-	
 		<div class='main' style="margin-top: 100px;">
 			<center>
 				<select class='selectpicker show-menu-arrow'>

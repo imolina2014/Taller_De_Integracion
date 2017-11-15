358b067f0a347ef56de721243bbf540e07624299
MarceloMontalba17 <mmontalba2015@alu.uct.cl>
2017-11-14 23:54:55 -0300

    Modificación filtrado de incidentes

--- ciudadano web/php/mostrarIncidentes.php
+++ ciudadano web/php/mostrarIncidentes.php
@@ -1,3 +1,22 @@
+<script>
+	function initMap(latitud,longitud,bandera) {
+		var uluru = {lat: latitud, lng: longitud};
+	    var map = new google.maps.Map(document.getElementById('map'), {
+	    	zoom: 16,
+	    	center: uluru
+	    });
+
+	    if(bandera==1){
+		    var marker = new google.maps.Marker({
+		    	position: uluru,
+		    	map: map
+		    });
+		}
+	}
+
+	initMap(-38.7396500,-72.5984200,0);
+</script>
+
 <?php
 	include("conex.php");
 	$calle=$_POST["Calle"];
@@ -6,54 +25,67 @@
 	$fecha=$_POST["Fecha"];
     
     
-    if ($calle=="")$sql = "SELECT CATEGORIA,TIPO,DESCRIPCION,FECHA,CALLE,AsText(COORDENADAS) FROM incidentes WHERE CATEGORIA='$categoria' AND TIPO='$tipo' AND FECHA='$fecha'";
+    if ($calle==""){
+    	if($fecha=="null"){
+    		$sql = "SELECT ID,CATEGORIA,TIPO,DESCRIPCION,FECHA,CALLE,AsText(COORDENADAS) FROM incidentes WHERE CATEGORIA='$categoria' AND TIPO='$tipo'";
+    	}
+    	else{
+    		$sql = "SELECT ID,CATEGORIA,TIPO,DESCRIPCION,FECHA,CALLE,AsText(COORDENADAS) FROM incidentes WHERE CATEGORIA='$categoria' AND TIPO='$tipo' AND FECHA='$fecha'";
+    	}
+    }
     
-	else $sql = "SELECT CATEGORIA,TIPO,DESCRIPCION,FECHA,CALLE,AsText(COORDENADAS) FROM incidentes WHERE CATEGORIA='$categoria' AND TIPO='$tipo' AND FECHA='$fecha' AND CALLE LIKE '%$calle%'";
+	else{
+		if($fecha=="null"){
+			$sql = "SELECT ID,CATEGORIA,TIPO,DESCRIPCION,FECHA,CALLE,AsText(COORDENADAS) FROM incidentes WHERE CATEGORIA='$categoria' AND TIPO='$tipo' AND CALLE LIKE '%$calle%'";
+		}
+		else{
+			$sql = "SELECT ID,CATEGORIA,TIPO,DESCRIPCION,FECHA,CALLE,AsText(COORDENADAS) FROM incidentes WHERE CATEGORIA='$categoria' AND TIPO='$tipo' AND CALLE LIKE '%$calle%' AND FECHA='$fecha'";
+		}
+	}
+
 	
 	$resultado = mysqli_query($mysqli,$sql);
     $nfilas = mysqli_num_rows($resultado);
 	
 	if($nfilas > 0){
-		echo "  <div class='row'>
-                <div class='col-md-6' style='border-radius: 2px; border: 2px solid #73AD21; overflow: scroll;'>
-                    <table class='table' style='width=100%'>
+		echo " 
+			<div class='jumbotron' style='margin-top:30px;background-color:#337ab7'>
+			<div class='row'>
+                <div class='col-md-6' style='overflow: scroll; overflow-x:hidden; height:400px;border-style: double; border-color:black;background-color:white'>
+                    <table class='table table-bordered' style='width:100%;'>
     				    <thead class='thead-inverse'>
             			    <tr>
-            				    <th>#</th>
+            				    <th>CODIGO</th>
             				    <th>TIPO</th>
             				    <th>FECHA</th>
             				    <th>UBICACIÓN</th>
             				</tr>
-            			</thead>
-            			<tbody>";
+            			</thead>";
 						
-	    $count = 1;
 		while($aDatos = mysqli_fetch_array($resultado)) {
 			echo "  <tr>    
-	                    <th scope='row'>$count</th>
-						<td><img src='img/icons/$aDatos[1].png' id='G_iconos'></td>
-						<td>$aDatos[3]</td>";
-			$CoorSep = $aDatos[5];
+	                    <td>$aDatos[0]</td>
+						<td><img src='img/icons/$aDatos[2].png' id='G_iconos' width=30 heigth=30></td>
+						<td>$aDatos[4]</td>";
+			
+			$CoorSep = $aDatos[6];
         	$CoorSep = explode(" ", $CoorSep);
         	$CoorSep[0]= explode("(", $CoorSep[0]);
         	$CoorSep[1]= explode(")", $CoorSep[1]);
         	$lat = $CoorSep[0][1];
         	$lon = $CoorSep[1][0];
-            echo "<td><input type='button' value='Ver Mapa' onclick='initMap($lat,$lon)' width='50' height='50' class='btn btn-primary'></td>
+
+            echo "<td><input type='button' value='Ver Mapa' onclick='initMap($lat,$lon,1)' width='50' height='50' class='btn btn-primary'></td>
 			     </tr>";
-			$count = $count + 1;
 		}
-		echo  "         </tbody>
-			        </table>
+		echo  "</table>
 			    </div>
-			    <div class='col-md-6' id='map' style='border-radius: 2px; border: 2px solid #73AD21;'><br>
-			        <img src='./img/maps.png' width='500' height='400'>
-			    </div>
-            </div>";
+			    <div class='col-md-6' id='map' style='border-style: double; border-color:black; height:400px'></div>
+            </div></div>";
 	}
-    
+  
 	else {
-	    echo "<script> alert('No se encontraron datos') </script>";
-		include("Incidentes.php");
+	    echo "<script> alert('No se encontraron datos'); 
+	    document.getElementById('Graficos').style.display='';</script>";
 	}
 ?>

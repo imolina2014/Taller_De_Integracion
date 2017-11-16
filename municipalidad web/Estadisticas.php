911992796a47ea6f2e004f3cc75e18f4186f60e9
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2017-11-15 23:11:18 -0300

    Borrando código innecesario

--- municipalidad web/Estadisticas.php
+++ municipalidad web/Estadisticas.php
@@ -109,39 +109,6 @@
 				}
 			});
 		});
-
-		$(document).ready(function(){
-			var ctx = document.getElementById("ChartBarIncidentes").getContext('2d');
-			var myChart = new Chart(ctx, {
-			    type: 'bar',
-			    data: {
-			        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-			        datasets: [
-			        {
-			            label: '#Accidentes',
-			            data: [12, 19, 3, 5, 2, 3],
-			            backgroundColor: [
-			                'rgba(255, 99, 132, 0.2)',
-			                'rgba(54, 162, 235, 0.2)'
-			            ],
-			            borderColor: [
-			                'rgba(255,99,132,1)',
-			                'rgba(54, 162, 235, 1)'
-			            ],
-			            borderWidth: 1
-			        }]
-			    },
-			    options: {
-			        scales: {
-			            yAxes: [{
-			                ticks: {
-			                    beginAtZero:true
-			                }
-			            }]
-			        }
-			    }
-			});
-		});
 		
 		$(document).ready(function(){
 			var ctx = document.getElementById("ChartBarIncidentes").getContext('2d');

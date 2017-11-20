d152a8637ee158369885f62d0436f76aa5c9dc05
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2017-11-20 15:04:50 -0300

    Eliminando Código No Usado

--- municipalidad web/js/index.js
+++ municipalidad web/js/index.js
@@ -1,30 +1,3 @@
-function mostrar() {
-	$.ajax({
-		type: "POST",
-		url: "datos.php",
-		success: function(d) {
-			$("#datos").html(d);
-		}
-	});	
-}
-
-function guardar() {
-	var datos = {
-		"Nombre": $("#caja_nombre").val(),
-		"Edad": $("#caja_edad").val()
-	}
-
-	$.ajax({
-		type: "POST",
-		url: "guardar.php",
-		data: datos,
-		success: function(d) {
-			alert(d);
-			mostrar();
-		}
-	});
-}
-
 function Crea_sector(){
 	var datos = {
 		"usuario": $("#nombre_usuario").val(),
